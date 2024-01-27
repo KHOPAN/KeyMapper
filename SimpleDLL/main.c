62e96ccab53ced167cfe9eeb404ade341b8069ec
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-01-28 00:28:03 +0700

    Add Even More Simple Function

--- SimpleDLL/main.c
+++ SimpleDLL/main.c
@@ -3,3 +3,7 @@
 __declspec(dllexport) void __stdcall SimpleFunction() {
 	MessageBoxW(NULL, L"SimpleFunction() has been called!", L"SimpleDLL", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
 }
+
+__declspec(dllexport) void __stdcall EvenMoreSimpleFunction() {
+	MessageBoxW(NULL, L"EvenMoreSimpleFunction() has been called!", L"SimpleDLL", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+}
