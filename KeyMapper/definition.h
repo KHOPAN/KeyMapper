5541c567ad8a775f99b671371b126d8a5c3cdaf1
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-01-27 19:34:04 +0700

    Add Config File Reader

---
+++ KeyMapper/definition.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Windows.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void DisplayError(DWORD, LPCWSTR);
+#ifdef __cplusplus
+}
+#endif
--- KeyMapper/main.c
+++ KeyMapper/main.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include <Windows.h>
+#include "definition.h"
 
 int main(int argc, char** argv) {
-	printf("Hello, world!\n");
+	HANDLE configFile = CreateFileW(L"keymapper.config.json", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	
+	if(configFile == INVALID_HANDLE_VALUE) {
+		DWORD error = GetLastError();
+
+		if(error == ERROR_FILE_NOT_FOUND) {
+			MessageBoxW(NULL, L"File 'keymapper.config.json' not found!", L"Error", MB_OK | MB_DEFBUTTON1 | MB_ICONERROR | MB_SYSTEMMODAL);
+			return error;
+		}
+
+		DisplayError(error, L"CreateFileW()");
+		return error;
+	}
+
+	CloseHandle(configFile);
 	return 0;
 }
