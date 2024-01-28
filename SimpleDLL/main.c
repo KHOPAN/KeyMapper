ca6688a7977f7f0165effdb69e70fade8825f9d3
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-01-28 15:27:00 +0700

    Test

--- SimpleDLL/main.c
+++ SimpleDLL/main.c
@@ -1,9 +1,5 @@
 #include <Windows.h>
 
-__declspec(dllexport) void __stdcall SimpleFunction() {
-	MessageBoxW(NULL, L"SimpleFunction() has been called!", L"SimpleDLL", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
-}
-
-__declspec(dllexport) void __stdcall EvenMoreSimpleFunction() {
-	MessageBoxW(NULL, L"EvenMoreSimpleFunction() has been called!", L"SimpleDLL", MB_OK | MB_ICONINFORMATION | MB_DEFBUTTON1 | MB_SYSTEMMODAL);
+__declspec(dllexport) void __stdcall OpenYouTube() {
+	system("start chrome https://www.youtube.com");
 }
