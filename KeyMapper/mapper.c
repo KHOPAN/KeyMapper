6da240cd58977b57b2282ec7201ec8027db57600
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-02-02 18:27:25 +0700

    Fix Memory Leak

--- KeyMapper/mapper.c
+++ KeyMapper/mapper.c
@@ -11,8 +11,13 @@ KeyMappings GetMappingData() {
 }
 
 void FreeMappingData() {
-	for(size_t i = 0; i < keyMappingList.keyMapSize; i++) {
-		KeyMapStruct keyMapping = keyMappingList.keyMapStruct[i];
+	for(size_t x = 0; x < keyMappingList.keyMapSize; x++) {
+		KeyMapStruct keyMapping = keyMappingList.keyMapStruct[x];
+
+		for(size_t y = 0; y < keyMapping.keyboardSize; y++) {
+			free(keyMapping.keyboardHuid[y]);
+		}
+
 		free(keyMapping.keyboardHuid);
 		free(keyMapping.mappings);
 	}
